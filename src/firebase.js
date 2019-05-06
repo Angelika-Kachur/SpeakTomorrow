c47ce142da5dd754123f546bfcc2c2ed201c5b4c
Angelika Kachur <angelika.kachur@gmail.com>
2019-05-06 20:13:50 +0300

    fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from './firebase';
 
 const config = {
-  apiKey: "AIzaSyDgwBLX17zGCYJTdXqBX0_POdQn_Q9_CH4",
-  authDomain: "speak-tomorrow-1553165359427.firebaseapp.com",
-  databaseURL: "https://speak-tomorrow-1553165359427.firebaseio.com",
-  projectId: "speak-tomorrow-1553165359427",
-  storageBucket: "speak-tomorrow-1553165359427.appspot.com",
-  messagingSenderId: "802530675333"
+    apiKey: "AIzaSyDgwBLX17zGCYJTdXqBX0_POdQn_Q9_CH4",
+    authDomain: "speak-tomorrow-1553165359427.firebaseapp.com",
+    databaseURL: "https://speak-tomorrow-1553165359427.firebaseio.com",
+    projectId: "speak-tomorrow-1553165359427",
+    storageBucket: "speak-tomorrow-1553165359427.appspot.com",
+    messagingSenderId: "802530675333"
 };
 
 firebase.initializeApp(config)
