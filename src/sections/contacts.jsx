ca8adaed47098c85c6d4fbe3d6979e4ec82f75ad
Angelika Kachur <angelika.kachur@gmail.com>
2019-08-24 08:31:02 +0300

    hide map instance

--- src/sections/contacts.jsx
+++ src/sections/contacts.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Map from "../components/Map";
+//import Map from "../components/Map";
 import TitleSection from "../components/TitleSection.jsx";
 
 var pageTitle = "Contacts";
