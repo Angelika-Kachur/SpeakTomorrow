1af7d15dc38222300105eb37874828b8f2afbcff
Angelika Kachur <angelika.kachur@gmail.com>
2019-08-25 23:16:10 +0300

    reviews section

--- src/sections/reviews.jsx
+++ src/sections/reviews.jsx
@@ -7,64 +7,65 @@ var pageTitle = "Вот что говорят наши студенты:";
 class reviewsPage extends Component {
   render() {
     return (
-      <div className="page-content" data-aos="fade-left">
-        <TitleSection title={pageTitle} />
-
-        <section id="reviews" className="reviews-section section">
-          <div className="holder">
-            <div className="reviews">
-              <div className="review">
-                <div className="tale">
-                  <div className="review-image">
-                    <img src={imageGroup1} alt="" />
-                  </div>
-                  <h2 className="review-name">Анастасия</h2>
-                  <h2 className="review-position">Project Manager</h2>
-                  <div className="review-text">
-                    <p>
-                      До занятий с Томой я едва ли могла связать слова вслух,
-                      наверное многим знакома проблема этого барьера. Слишишь,
-                      даже что-то понимаешь, а сказать не можешь
-                    </p>
-                  </div>
+      <section
+        id="reviews"
+        className="reviews-section section"
+        data-aos="fade-left"
+      >
+        <div className="holder">
+          <TitleSection title={pageTitle} />
+          <div className="reviews">
+            <div className="review">
+              <div className="tale">
+                <div className="review-image">
+                  <img src={imageGroup1} alt="" />
+                </div>
+                <h2 className="review-name">Анастасия</h2>
+                <h2 className="review-position">Project Manager</h2>
+                <div className="review-text">
+                  <p>
+                    До занятий с Томой я едва ли могла связать слова вслух,
+                    наверное многим знакома проблема этого барьера. Слишишь,
+                    даже что-то понимаешь, а сказать не можешь
+                  </p>
                 </div>
               </div>
-              <div className="review">
-                <div className="tale">
-                  <div className="review-image">
-                    <img src={imageGroup1} alt="" />
-                  </div>
-                  <h2 className="review-name">Анастасия</h2>
-                  <h2 className="review-position">Project Manager</h2>
-                  <div className="review-text">
-                    <p>
-                      До занятий с Томой я едва ли могла связать слова вслух,
-                      наверное многим знакома проблема этого барьера. Слишишь,
-                      даже что-то понимаешь, а сказать не можешь
-                    </p>
-                  </div>
+            </div>
+            <div className="review">
+              <div className="tale">
+                <div className="review-image">
+                  <img src={imageGroup1} alt="" />
+                </div>
+                <h2 className="review-name">Анастасия</h2>
+                <h2 className="review-position">Project Manager</h2>
+                <div className="review-text">
+                  <p>
+                    До занятий с Томой я едва ли могла связать слова вслух,
+                    наверное многим знакома проблема этого барьера. Слишишь,
+                    даже что-то понимаешь, а сказать не можешь
+                  </p>
                 </div>
               </div>
-              <div className="review">
-                <div className="tale">
-                  <div className="review-image">
-                    <img src={imageGroup1} alt="" />
-                  </div>
-                  <h2 className="review-name">Анастасия</h2>
-                  <h2 className="review-position">Project Manager</h2>
-                  <div className="review-text">
-                    <p>
-                      До занятий с Томой я едва ли могла связать слова вслух,
-                      наверное многим знакома проблема этого барьера. Слишишь,
-                      даже что-то понимаешь, а сказать не можешь
-                    </p>
-                  </div>
+            </div>
+            <div className="review">
+              <div className="tale">
+                <div className="review-image">
+                  <img src={imageGroup1} alt="" />
+                </div>
+                <h2 className="review-name">Анастасия</h2>
+                <h2 className="review-position">Project Manager</h2>
+                <div className="review-text">
+                  <p>
+                    До занятий с Томой я едва ли могла связать слова вслух,
+                    наверное многим знакома проблема этого барьера. Слишишь,
+                    даже что-то понимаешь, а сказать не можешь
+                  </p>
                 </div>
               </div>
             </div>
           </div>
-        </section>
-      </div>
+        </div>
+      </section>
     );
   }
 }
