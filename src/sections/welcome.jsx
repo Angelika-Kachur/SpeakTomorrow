257cffae5ba092ac4829dccbdaaba4f27d7ca201
Angelika Kachur <angelika.kachur@gmail.com>
2019-10-20 15:38:57 +0300

    Welcome extra deleted

--- src/sections/welcome.jsx
+++ src/sections/welcome.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import TitleSection from "../components/TitleSection.jsx";
 
 class welcome extends Component {
   render() {
@@ -8,25 +7,6 @@ class welcome extends Component {
         <div className="image">
           <div class="title-holder">
             <h1 class="title">Let’s speak tomorrow!</h1>
-          </div>
-        </div>
-        <div className="holder">
-          <div className="text">
-            <p>
-              Speak Tomorrow это школа английского, где вас научат говорить
-              уверенно.
-            </p>
-            <p>
-              Главным в работе со студентами мы считаем индивидуальный подход к
-              каждому. Английский должен помогать решать Ваши задачи. Именно
-              поэтому, мы учитываем Вашу специфику работы и Ваши личные цели.
-            </p>
-            <p>
-              Мы научим вас общаться, объясняться, спорить, шутить и доказывать
-              свою точку зрения. Вы будете жить на английском и считать его
-              частью своей жизни.
-            </p>
-            <button className="btn btn-courses">Наши курсы</button>
           </div>
         </div>
       </section>
